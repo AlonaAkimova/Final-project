c34438280a3ee99298e5214840837fa2e90b8ad0
AlonaAkimova <aliona.akimova@gmail.com>
2023-03-07 17:49:33 +0700

    update

--- happystay_landing/src/pages/blog/BlogList.js
+++ happystay_landing/src/pages/blog/BlogList.js
@@ -7,8 +7,8 @@ export default function BlogList({ articles }) {
     <div className="section-blog">
       {articles.map((article) => (
         <div key={article.id}>
-          <h3>{article.title}</h3>
-          <p>{article.content}</p>
+          <h3 className="heading-secondary">{article.title}</h3>
+          <p className="cta-text">{article.content}</p>
         </div>
       ))}
     </div>
