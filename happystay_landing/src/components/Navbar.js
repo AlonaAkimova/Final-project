c477304b57c524293c38052d5761d230d6f7b41e
AlonaAkimova <aliona.akimova@gmail.com>
2023-03-08 18:18:10 +0700

    update

--- happystay_landing/src/components/Navbar.js
+++ happystay_landing/src/components/Navbar.js
@@ -1,18 +1,31 @@
 import "./Navbar.css";
-
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
+  const [showNav, setShowNav] = useState(false);
+
+  function toggleNav() {
+    setShowNav(!showNav);
+  }
+
+  function hideNav() {
+    setShowNav(false);
+  }
+
   return (
     <div className="header">
       <div className="navbar">
-        <nav>
+        <nav onMouseLeave={hideNav}>
           <Link to="/">
             <h1 className="logo">Happy Stay</h1>
           </Link>
-          <div className="main-nav">
+          <button className="btn-mobile-nav" onClick={toggleNav}>
+            <FontAwesomeIcon icon={faBars} className="icon-mobile-nav" />
+          </button>
+          <div className={showNav ? "main-nav show" : "main-nav"}>
             <Link to="/about">Jak to działa</Link>
             <Link to="/blog">Blog</Link>
             <Link to="/contact" className="contact-link">
@@ -21,12 +34,6 @@ export default function Navbar() {
           </div>
         </nav>
       </div>
-      <button className="btn-mobile-nav">
-        <FontAwesomeIcon
-          icon="fa-sharp fa-regular fa-bars"
-          className="icon-mobile-nav"
-        />
-      </button>
     </div>
   );
 }
