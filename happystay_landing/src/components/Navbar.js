6948ec192229a4b4ab6cba4f0f664c7abd4a45ce
AlonaAkimova <aliona.akimova@gmail.com>
2023-03-08 11:27:23 +0700

    updates

--- happystay_landing/src/components/Navbar.js
+++ happystay_landing/src/components/Navbar.js
@@ -2,22 +2,31 @@ import "./Navbar.css";
 
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-      <nav>
-        <Link to="/">
-          <h1 className="logo">Happy Stay</h1>
-        </Link>
-        <div className="main-nav">
-          <Link to="/about">Jak to działa</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact" className="contact-link">
-            Kontakt
+    <div className="header">
+      <div className="navbar">
+        <nav>
+          <Link to="/">
+            <h1 className="logo">Happy Stay</h1>
           </Link>
-        </div>
-      </nav>
+          <div className="main-nav">
+            <Link to="/about">Jak to działa</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/contact" className="contact-link">
+              Kontakt
+            </Link>
+          </div>
+        </nav>
+      </div>
+      <button className="btn-mobile-nav">
+        <FontAwesomeIcon
+          icon="fa-sharp fa-regular fa-bars"
+          className="icon-mobile-nav"
+        />
+      </button>
     </div>
   );
 }
